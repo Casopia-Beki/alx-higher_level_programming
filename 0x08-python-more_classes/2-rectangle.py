e1b5d9187c08ee26f2a7d45df46e6044ca47116c
Casopia-Beki <bereketkassaye@gmail.com>
2023-04-03 12:51:20 -0700

    Area and Perimeter

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Define a class Rectangle."""
+
+
+class Rectangle:
+    """Represent a rectangle."""
+
+    def __init__(self, size=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            size (int): The size of the new rectangle.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
